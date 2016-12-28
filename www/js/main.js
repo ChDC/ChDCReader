e20e2880356f6a17f2c95f5ee2e98157797fcc1b
Chen Dacai <chendacai@qq.com>
2016-12-28 13:33:02 +0800

    Update Resources

--- www/js/main.js
+++ www/js/main.js
@@ -25,6 +25,40 @@ define(["jquery", "util", "book", "page", "bootstrap"], function($, util, book, 
             page.init();
             // self.page = page;
             page.showPage("bookshelf");
+            this.chekcUpdate();
+        },
+        chekcUpdate: function(){
+            function fetchUpdateCallback(error, data) {
+                if (error) {
+                    util.showMessage('加载资源更新失败！错误码：' + error.code);
+                    console.error(error.description);
+                    return;
+                }
+                util.showMessage('资源更新已下载，下次启动时生效！');
+            },
+
+            function installationCallback(error) {
+                if (error) {
+                    util.showMessage('安装资源更新失败！错误码：' + error.code);
+                    console.error(error.description);
+                }
+                else {
+                    util.showMessage('资源更新安装成功！');
+                }
+            }
+            // check, if update was previously loaded and available for download
+            chcp.isUpdateAvailableForInstallation(function(error, data) {
+                if (error) {
+                    util.showMessage('开始获取资源更新。。。');
+                    chcp.fetchUpdate(fetchUpdateCallback);
+                    return;
+                }
+
+                // update is in cache and can be installed - install it
+                console.log('Current version: ' + data.currentVersion);
+                console.log('About to install: ' + data.readyToInstallVersion);
+                chcp.installUpdate(installationCallback);
+            });
         }
     };
     app.init();
--- www/js/test.js
+++ www/js/test.js
@@ -5,17 +5,52 @@ define(["../lib/jquery-3.1.1/jquery.min", "util"], function($, util) {
 
     // 从副列表中匹配查询主列表的元素
     function listMatch(mainArray, viceArray, index, compareFunction){
+
+        // 比较邻居
+        function compareNeighbor(index){
+
+        }
+
+        // 提供最优结果
+        // 最终从所有结果中选出一个最好的
+        var result = [];
         var i, j;
+
+        var item = mainArray[index];
+
         i = util.arrayIndex(viceArray, item, compareFunction);
+        if(i < 0){
+            // 没找到一个结果
+            return -1;
+        }
+        // 找到结果，开始分析
+        // 比对前邻和后邻是否相同
+        result.append({
+            index: i,
+        });
+
         return i;
     }
 
     var lm = ['A', 'B', 'C', 'D', 'E', 'G'];
-    var lv = ['0', 'A', 'B', 'E', 'D', 'F', 'B'];
+    var lv = ['0', 'B', 'A', 'B', 'E', 'D', 'F', 'B'];
 
     function compare(ia, ib){
         return ia == ib;
     }
 
-    console.log(listMatch(lm, lv, 'A', compare));
+    function print(title, gi, ri){
+        var s = "Compare" + title + ": " + gi + ', ' + 'ri';
+        if(gi == ri)
+            console.log(s);
+        else
+            console.error(s);
+    }
+    print('A', listMatch(lm, lv, 0, compare), 2);
+    print('B', listMatch(lm, lv, 1, compare), 3); // 重复元素
+    print('D', listMatch(lm, lv, 3, compare), 5); // 乱序元素
+    print('E', listMatch(lm, lv, 4, compare), 4); // 乱序元素
+    print('G', listMatch(lm, lv, 5, compare), -1); // 不存在元素
+
+
 });
