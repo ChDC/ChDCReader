69d14d758587d5e7360ca8317bf4273510b71176
Chen Dacai <chendacai@qq.com>
2017-01-03 08:43:50 +0800

    Update Resources

--- tools/update.bat
+++ tools/update.bat
@@ -1,3 +1,3 @@
 cd ..
-cordova-hcp build && git add . && git commit -m "Update Resources" && git push github master && python RefreshGithubCachePage.py
+cordova-hcp build && git add . && git commit -m "Update Resources" && git push github master && python tools/RefreshGithubCachePage.py
 pause
