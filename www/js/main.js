f4d4f86db57bd01daedb592d012060c2e54640a0
Chen Dacai <chendacai@qq.com>
2017-01-08 12:22:38 +0800

    Update Resources

--- www/js/main.js
+++ www/js/main.js
@@ -36,7 +36,10 @@ define(["jquery", "util", "book", "page", "bookshelf", "bootstrap"], function($,
                 return;
             function fetchUpdateCallback(error, data) {
                 if (error) {
-                    if(error.code != 2){
+                    if(error.code == 2){
+                        util.showMessage('没有更新');
+                    }
+                    else{
                         var errMsg = error.description + "(" + error.code + ")";
                         util.error('Fail to download update: ' + errMsg);
                         util.showMessage('更新下载失败！\n' + errMsg);
