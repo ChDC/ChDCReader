27da1c72cf5ece40bb89fd3105cf9f19f7e3bfa5
Chen Dacai <chendacai@qq.com>
2017-04-18 14:17:58 +0800

    fix tools

--- tools/UpdateResources.bat
+++ tools/UpdateResources.bat
@@ -5,4 +5,4 @@ set branch=dev
 set url=https://raw.githubusercontent.com/ChDC/ChDCNovelReader/dev/
 echo Branch on %branch% with url %url%
 
-git checkout %branch% && python tools/ChangeEnv.py -e debug && npm run build && python tools/ReplaceCRLFtoLF.py -d www && cordova-hcp build && git add . && git commit -m "Update Resources" && git push github %branch% && python tools/RefreshGithubURL.py -d www -u %url%
+git checkout %branch% && npm run build && python tools/ReplaceCRLFtoLF.py -d www && cordova-hcp build && git add . && git commit -m "Update Resources" && git push github %branch% && python tools/RefreshGithubURL.py -d www -u %url%
