b3d25cf1bebfe31f02ba70e84879f1d7daad1c04
Chen Dacai <chendacai@qq.com>
2017-05-01 00:32:25 +0800

    change tools

--- tools/UpdateReleaseResources.bat
+++ tools/UpdateReleaseResources.bat
@@ -5,4 +5,4 @@ set branch=master
 set url=https://raw.githubusercontent.com/ChDC/ChDCReader/master/
 echo Branch on %branch% with url %url%
 
-git checkout %branch% && git merge dev && npm run compile && python tools/ReplaceCRLFtoLF.py -d www && cordova-hcp build && git add . && git commit -m "Update Resources" && git push github %branch% && python tools/RefreshGithubURL.py -d www -u %url%
+git checkout %branch% && git merge dev && npm run compile && python tools/ReplaceCRLFtoLF.py -d www && cordova-hcp build && git add . && git commit -m "Update Resources" && git push github %branch% && git checkout dev && python tools/RefreshGithubURL.py -d www -u %url%
